69a169d35d45b1847ddf3f1b05fb87f07514fc3f
Renato <renatoandre2000@hotmail.com>
2020-11-08 19:19:49 +0000

    Alterações

---
+++ Ficha4/a.java
@@ -0,0 +1,13 @@
+class Barrier{
+	private int n;
+	private int nr_threads;	
+	private Lock l = new ReentrantLock();
+
+	Barrier (int N) { 
+
+	}
+
+	void await() throws InterruptedException {
+
+	}
+}
