e9a5317985ee487e1764ad3a94e16b2fc69a788e
Renato <renatoandre2000@hotmail.com>
2020-11-08 19:18:37 +0000

    Alterações e correções

--- Ficha5/Warehouse.java
+++ Ficha5/Warehouse.java
@@ -108,9 +108,88 @@ class WarehouseV2 {
   			}
   			//2. Consumo
   			//Só chegamos a esta fase se nunca cairmos no await, ou seja, se todos os produtos estiverem disponiveis
+  			System.out.println(Thread.currentThread().getName() + " getting items");
   		   	for (String s : a){
   		   		Product p = get(s);
   		  		p.q--;
+  		  	}
+  		}
+  		finally { l.unlock(); }
+  	}
+}
+
+/*
+Versão com um ponto de tolerancia, onde o cliente só deixa passar um certo nr de clientes até passar à abordagem egoista.
+*/
+class WarehouseV3 {
+  	private Lock l = new ReentrantLock();
+  	private Map<String, Product> m =  new HashMap<String, Product>();
+  	private int max = 10; //Nr maximo de clientes que deixa passar à frente
+
+  	private class Product { 
+  		int q = 0; 
+  		/*
+  		Para que threads não acordem desnecessariamente, podemos colocar uma condição em cada produto, o que 
+  		garante que apenas threads que estejam interessadas neste produto é que vão acordar.
+  		*/
+  		Condition isEmpty = l.newCondition();
+  	}
+
+ 	private Product get(String s) {
+	    Product p = m.get(s);
+ 	   	if (p != null) return p;
+ 	   	p = new Product();
+ 	    m.put(s, p);
+  	    return p;
+  	}
+
+  	public void supply(String s, int q) {
+  		l.lock();
+  	  	try{
+  	  	  	Product p = get(s);
+  	  	  	p.q += q;
+  	  	  	p.isEmpty.signalAll();
+  	  	}
+  		finally{ l.unlock(); }
+  	}
+
+  	public void consume(String[] a) throws InterruptedException {
+  		l.lock();
+  		try{
+  			//1. Verificação
+  			int i = 0;
+  			int n = 0;
+  			while(i < a.length && n < max){
+  		   		Product p = get(a[i]);
+
+  		   		i++;
+  				while(p.q == 0) {
+  		  			System.out.println(Thread.currentThread().getName() + " waiting for item" + a[i]);
+  		  			p.isEmpty.await();
+  		  			i=0; //Como algo pode ter alterado depois do await, temos de verificar de novo
+  		  			n++;
+  		  		}
+  			}
+  			if(n < max){ //Continua com a abordagem cooperativa
+  				//2. Consumo
+  				//Só chegamos a esta fase se nunca cairmos no await, ou seja, se todos os produtos estiverem disponiveis
+  				System.out.println(Thread.currentThread().getName() + " getting items");
+  		   		for (String s : a){
+  		   			Product p = get(s);
+  		  			p.q--;
+
+  				}
+  		  	}
+  		  	else{ //Passa para a abordagem egoista
+  		   		for (String s : a){
+  		   			Product p = get(s);
+  		  			while(p.q == 0) {
+  		  				System.out.println(Thread.currentThread().getName() + " waiting for item" + s);
+  		  				p.isEmpty.await();
+  		  			} 
+  		  			p.q--;
+  		  			System.out.println(Thread.currentThread().getName() + " got item " + s);
+  		  		}
   		  	}
   		}
   		finally { l.unlock(); }
