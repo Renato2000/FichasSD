6fe0af3c081f742d436abbb41e826b98cfe00a97
Renato <renatoandre2000@hotmail.com>
2020-12-16 14:30:33 +0000

    Ficha 9

--- Ficha8/Demultiplexer.java
+++ Ficha8/Demultiplexer.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class Demultiplexer implements AutoCloseable {
 	private final TaggedConnection conn;
 	private final Map<Interger, Entry> buffer = new HashMap<>();
-	private final ReentranteLock lock = new ReentranteLock();
+	private final ReentrantLock lock = new ReentrantLock();
 	private Exception exception;
 	
 	private class Entry{
@@ -30,26 +30,32 @@ public class Demultiplexer implements AutoCloseable {
 	public void start() {
 		new Thread(() -> { //nesta notação, as threads vão partilhar as variáveis criadas no metodo start
 			try{
-				// 1. ler frame da connection
-				Frame frame = this.conn.receive();
+				while(true) {
 
-				//Este lock pode estar depois do receive, pq só temos uma thread a ler o receive, e o seu conteudo está protegido
-				lock.lock();
+					// 1. ler frame da connection
+					Frame frame = this.conn.receive();
 
-				try {
-					// 2. ler tag da frame, para obter a entrada correspondente e inserir na queue correspondente
-					Entry e = get(frame.tag);
-					e.queue.add(frame.data);
+					//Este lock pode estar depois do receive, pq só temos uma thread a ler o receive, e o seu conteudo está protegido
+					lock.lock();
 
-					// 3, notificar thread que está a aguardar por mensagens
-					e.cond.signal();
-				} finally { lock.unlock(); }
+					try {
+						// 2. ler tag da frame, para obter a entrada correspondente e inserir na queue correspondente
+						Entry e = get(frame.tag);
+						e.queue.add(frame.data);
+
+						// 3, notificar thread que está a aguardar por mensagens
+						e.cond.signal();
+					} finally { lock.unlock(); }
+				}
 			}
 			catch(IOException e) {
-				this.exception = e;
-
 				//sinalizar todas as threads
+				lock.lock(); //é necessário adequiri o lock para usar a variável de instancia
+				this.exception = e; //é melhor ficar dentro do lock, porque é um estado partilhado, e evita que novas threads entrem no while
 				buffer.forEach((k, v) -> v.cond.signalAll());
+				lock.unlock();
+
+				System.out.println("Something went wrong");
 			}
 		}).start();
 	} 
@@ -70,12 +76,17 @@ public class Demultiplexer implements AutoCloseable {
 			Entry e = get(tag);
 
 			// 2. enquanto mão houver mensagens para ler da queue dessa entrada, bloqueia
-			while(e.queue.isEmpty()) {
+			while(e.queue.isEmpty() && this.exception == null) {
 				e.cond.await();
 			}
 
-			// 3. retorna os dados
-			return e.queue.poll();
+			//2.1. verificar se há mensagens para ler...
+			if(!e.queue.isEmpty()){
+				return e.queue.poll();
+			}
+
+			//2.2 verificar o erro...
+			throw this.exception;
 		} finally { lock.unlock(); }
 	}
 
